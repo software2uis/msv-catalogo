6338ed53daedca7056772f98649b44939fdc2196
Majinka10 <camachopinto_@hotmail.com>
2024-10-05 21:28:43 -0500

    update change to prove CODEOWNERS

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 // esta es una prueba para ver si (i) los asigna como reviewers y (ii) agrega el label de manera automática
 // otro cambio para ver si funciona
+// un cambio más para probar los CODEOWNERS
