125b86aaccc7310117ab9dbad89e0c1ba392f5bd
Majinka10 <camachopinto_@hotmail.com>
2024-10-05 21:43:11 -0500

    update to test auto assign

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 // esta es una prueba para ver si (i) los asigna como reviewers y (ii) agrega el label de manera automática
 // otro cambio para ver si funciona
 // un cambio más para probar los CODEOWNERS
+// cambio para probar el auto assign
