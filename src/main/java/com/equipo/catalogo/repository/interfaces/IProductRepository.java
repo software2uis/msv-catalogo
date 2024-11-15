cbf24e01c7910c8ce6fb60b0af8372b0ed4c9bcd
Carlos <ceguito06@gmail.com>
2024-11-15 12:07:21 -0500

    Add: Endpoint for Suggestions

--- src/main/java/com/equipo/catalogo/repository/interfaces/IProductRepository.java
+++ src/main/java/com/equipo/catalogo/repository/interfaces/IProductRepository.java
@@ -3,8 +3,11 @@ package com.equipo.catalogo.repository.interfaces;
 import com.equipo.catalogo.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 
 @Repository
 public interface IProductRepository extends MongoRepository<Product,String> {
+
+    List<Product> findByNameContainingIgnoreCase(String query);
 }
